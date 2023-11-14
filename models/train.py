dd943557eae81119f93ca139dba3c55c5dcf1c43
Sarvam Maheshwari <sarvam.mah31@gmail.com>
2023-11-14 13:42:33 +0530

    adding train.py

--- models/train.py
+++ models/train.py
@@ -1,14 +1,20 @@
-from utils import *
+from utils import load_flattened_data
 from classifier import *
 
-X_train, y_train = utils.load_flattened_data(DATA_PATH, SPECIES, split = 'train')
+X_train, y_train = load_flattened_data(DATA_PATH, SPECIES, split = 'train')
 
 TRAINED_MODELS_PATH = os.path.join(os.getcwd(), 'models/trained_models')
 
-def main():
+def main() -> None:
+    """Trains Random Forest and SVM classifiers and saves the models.
+
+    Raises:
+    - Any exceptions that might occur during training.
+    """
     train_RF_classifier(X_train, y_train, TRAINED_MODELS_PATH)
     train_SVM_classifier(X_train, y_train, TRAINED_MODELS_PATH)
     # train_CNN_classifier(X_train, y_train, trained_models_path)
+
 
 
 if __name__ == "__main__":
